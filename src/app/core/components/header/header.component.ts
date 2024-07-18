2a42f1f089bb7255881e098f13d85da7dbfb517d
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-07-18 19:08:33 -0300

    criacao e desenvolvimento do enum

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ETheme } from './enums/ETheme.enum';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public icon: string = ETheme.ICON_MOON;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public toggle() {
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if (theme) {
+      return (this.icon = ETheme.ICON_SUN);
+    }
+
+    return (this.icon = ETheme.ICON_MOON);
+  }
 }
