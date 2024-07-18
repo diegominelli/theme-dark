ac453ad24993180b8b902a00dfafeb5cc3d786b1
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2024-07-18 18:42:29 -0300

    criando as variaveis do css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'theme-dark-angular';
-}
+export class AppComponent {}
